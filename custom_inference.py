d52c888879ad9accd2029a644a695184788c3a37
Watson <watsonchua@gmail.com>
2024-09-03 03:27:51 +0000

    add text streaming

--- custom_inference.py
+++ custom_inference.py
@@ -1,8 +1,8 @@
-from transformers import AutoModelForCausalLM, BitsAndBytesConfig, AutoTokenizer
+from transformers import AutoModelForCausalLM, BitsAndBytesConfig, AutoTokenizer, TextIteratorStreamer
 import torch
 from peft import PeftModel
 from fire import Fire
-
+from threading import Thread
 
 
 from fastapi import FastAPI, HTTPException
@@ -15,17 +15,28 @@ import yaml
 device = torch.device("cuda")
 
 def generate_with_model(eval_prompt, ft_model, eval_tokenizer, temperature, repetition_penalty, custom_stop_tokens, max_new_tokens):
-    model_input = eval_tokenizer(eval_prompt, return_tensors="pt").to(device)
+    streamer = TextIteratorStreamer(eval_tokenizer)
+    model_inputs = eval_tokenizer(eval_prompt, return_tensors="pt").to(device)
+    generation_kwargs = dict(model_inputs, streamer=streamer, max_new_tokens=max_new_tokens, repetition_penalty=repetition_penalty, tokenizer=eval_tokenizer, temperature=temperature)
+    if custom_stop_tokens is not None:
+        generation_kwargs["stop_strings"] = custom_stop_tokens
+    thread = Thread(target=ft_model.generate, kwargs=generation_kwargs)
+    thread.start()
 
-    with torch.no_grad():
-        if custom_stop_tokens is None:
-            model_output = ft_model.generate(**model_input, max_new_tokens=max_new_tokens, repetition_penalty=repetition_penalty, temperature=temperature)[0]
-        else:
-            model_output = ft_model.generate(**model_input, max_new_tokens=max_new_tokens, repetition_penalty=repetition_penalty, stop_strings=custom_stop_tokens.split(","), tokenizer=eval_tokenizer, temperature=temperature)[0]
+    generated_text = ""
+    for new_text in streamer:
+        generated_text += new_text
+        yield generated_text
 
-        text_output = eval_tokenizer.decode(model_output, skip_special_tokens=False)
+#    with torch.no_grad():
+#        if custom_stop_tokens is None:
+#            model_output = ft_model.generate(**model_input, max_new_tokens=max_new_tokens, repetition_penalty=repetition_penalty, temperature=temperature)[0]
+#        else:
+#            model_output = ft_model.generate(**model_input, max_new_tokens=max_new_tokens, repetition_penalty=repetition_penalty, stop_strings=custom_stop_tokens, tokenizer=eval_tokenizer, temperature=temperature)[0]
+
+#        text_output = eval_tokenizer.decode(model_output, skip_special_tokens=False)
 #        print(text_output)
-        return text_output
+#        return text_output
 
 
 
@@ -79,27 +90,49 @@ def load_model(cfg, lora_model_dir):
 
 
 
-def main(config_path, lora_model_dir, custom_stop_tokens=""):
+def main(config_path, lora_model_dir):
     with open(config_path, 'r') as f:
         cfg = yaml.load(f, Loader=yaml.Loader)
     finetuned_model, eval_tokenizer = load_model(cfg, lora_model_dir)
 
-    print("Enter/Paste your instruction. Ctrl-D to predict.")
+#   custom_stop_tokens = cfg.get("stop_tokens", None)
 
-    while True:
-        contents = []
-        while True:
-            try:
-                line = input()
-            except EOFError:
-                break
-            contents.append(line)
-        prompt = '\n'.join(contents)
-        if not prompt.strip():
-            break
-        output = generate_with_model(eval_prompt=prompt, ft_model=finetuned_model, eval_tokenizer=eval_tokenizer, temperature=0.01, repetition_penalty=1.15, max_new_tokens=500, custom_stop_tokens=custom_stop_tokens)
-        print(output)
+#    print("Enter/Paste your instruction. Ctrl-D to predict.")
 
+#    while True:
+#        contents = []
+#        while True:
+#            try:
+#                line = input()
+#            except EOFError:
+#                break
+#            contents.append(line)
+#        prompt = '\n'.join(contents)
+#        if not prompt.strip():
+#            break
+#        output = generate_with_model(eval_prompt=prompt, ft_model=finetuned_model, eval_tokenizer=eval_tokenizer, temperature=0.01, repetition_penalty=1.15, max_new_tokens=500, custom_stop_tokens=custom_stop_tokens)
+#        print(output)
+
+    def gradio_generate(prompt):
+        yield from generate_with_model(eval_prompt=prompt, ft_model=finetuned_model, eval_tokenizer=eval_tokenizer, temperature=cfg.get("gradio_temperature", 0.01), repetition_penalty=1.15, max_new_tokens=cfg.get("inference_max_new_tokens",500), custom_stop_tokens=cfg.get("stop_strings"))
+
+
+    import gradio as gr
+    
+    demo = gr.Interface(
+        fn=gradio_generate,
+        inputs="textbox",
+        outputs="textbox",
+        title="Axolotl Gradio Interface",
+    )
+
+
+    demo.queue().launch(
+        show_api=True,
+        share=cfg.get("gradio_share", False),
+        server_name=cfg.get("gradio_server_name", "0.0.0.0"),
+        server_port=cfg.get("gradio_server_port", 8501),
+    )
 
 
 if __name__ == "__main__":
